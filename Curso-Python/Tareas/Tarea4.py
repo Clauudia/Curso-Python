7171a119e64204e342433f6938aeaa27faf3c465
Claudia <claugaro@ciencias.unam.mx>
2017-12-15 00:00:19 -0600

    :s

--- Curso-Python/Tareas/Tarea4.py
+++ Curso-Python/Tareas/Tarea4.py
@@ -38,64 +38,63 @@ def lee_xml(archivo_passwd):
 		root = ET.fromstring(nmap.read())
   		for n in root.findall('host'):  #Pueros abieros
         	hpr = n.find('status').get('state')
-		if hpre == 'up':
-			hpre.append(hpre)
-       		ha = n.find('status').get('state')
-		if ha == 'down':
-			hapa.append(ha)
-		h2 = n.find('port').get('portid')
-		a = n.find('state').get('state')
+			if hpre == 'up':
+				hpre.append(hpre)
+       			ha = n.find('status').get('state')
+			if ha == 'down':
+				hapa.append(ha)
+				h2 = n.find('port').get('portid')
+				a = n.find('state').get('state')
 			if h2 == '22' and a == "open":
 				h22.append(h2)
-			h5 = n.find('port').get('portid')
-			a = n.find('state').get('state')
-				if h5 == '53' and a == "open":
-					h53.append(h5)
-			h8 = n.find('port').get('portid')
-			a = n.find('state').get('state')
-				if h8 == '80' and a == "open":
-					h80.append(h8)
-			h4 = n.find('port').get('portid')
-			a = n.find('state').get('state')
-				if h4 == '443' and a == "open":
-					h443.append(h4)
-			d = n.find('hostname').get('name')
-			hd.append(d)
-			var = n.find('service').get('product') #Servicios http
-				if var == 'nginx':
-					hn.append(var)
-			vaa = n.find('service').get('product')
+				h5 = n.find('port').get('portid')
+				a = n.find('state').get('state')
+			if h5 == '53' and a == "open":
+				h53.append(h5)
+				h8 = n.find('port').get('portid')
+				a = n.find('state').get('state')
+			if h8 == '80' and a == "open":
+				h80.append(h8)
+				h4 = n.find('port').get('portid')
+				a = n.find('state').get('state')
+			if h4 == '443' and a == "open":
+				h443.append(h4)
+				d = n.find('hostname').get('name')
+				hd.append(d)
+				var = n.find('service').get('product') #Servicios http
+			if var == 'nginx':
+				hn.append(var)
+				vaa = n.find('service').get('product')
 				if vaa == 'Apache':
 					ha.append(vaa)
-			vd = n.find('service').get('product')
+					vd = n.find('service').get('product')
 				if vd == 'Dionaea':
 					hdio.append(vd)
-			o = n.find(('service').get('product')
+					o = n.find(('service').get('product')
 				if o != 'Apache' and o != 'Dionaea' and o != 'nginx':
 					ho.append(o)
-			ipa = n.find(('address').get('addr')   #Segunda parte del examen
-			hpr = n.find('status').get('state')
+					ipa = n.find(('address').get('addr')   #Segunda parte del examen
+					hpr = n.find('status').get('state')
 				if hpr == 'down':
 					iapa.append(ipa)
-			ipr = n.find(('address').get('addr')
-			hpr = n.find('status').get('state')
+					ipr = n.find(('address').get('addr')
+					hpr = n.find('status').get('state')
 				if hpr == 'up':
 					ipren.append(ipr)
-			i2 = n.find(('address').get('addr')
-			h2 = n.find('port').get('portid')
-			a = n.find('state').get('state')
+					i2 = n.find(('address').get('addr')
+					h2 = n.find('port').get('portid')
+					a = n.find('state').get('state')
 				if h2 == '22' and a == "open":
 					i22.append(i2)
-			iho = n.find(('address').get('addr')
-			hs = n.find('service').get('name')
+					iho = n.find(('address').get('addr')
+					hs = n.find('service').get('name')
 				if hs == 'honeypot':
 					ih.append(iho)
-			idom = n.find(('address').get('addr')
-			hn = n.find('hostname').get('name')
+					idom = n.find(('address').get('addr')
+					hn = n.find('hostname').get('name')
 					ido.append(idom)
 
 #Creacion archivos csv
-
 file = open("apagados.csv", "w". newline='')
 spamreader = csv.writer(file)
 spamreader = writerow(iapa)
